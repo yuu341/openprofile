a2614f3e9c299ae2215bd80a7fbf0d0e39a18fb1
Yusuke Furuya <yusuke.furuya.tech@gmail.com>
2022-05-07 23:36:39 +0900

    add firebase props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth, sendSignInLinkToEmail } from 'firebase/auth';
+
+const env = process.env;
+
+export const cfg = {
+    apiKey: env.REACT_APP_API_KEY,
+    authDomain: env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: env.REACT_APP_DATABASE_URL,
+    projectId: env.REACT_APP_PROJECT_ID,
+    storageBucket: env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: env.REACT_APP_APP_ID,
+    measurementId: env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(cfg);
+const analytics = getAnalytics(app);
+const auth = getAuth();
 
 function App() {
   return (
